39ab7e62a13cdc839969ac01fe3f80a6c0b14078
gabrielasandor <71887713+gabrielasandor@users.noreply.github.com>
2021-02-28 18:49:09 +0200

    fetch

---
+++ t-shirt-shop/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Product from './Product.js';
+import tshirts from './tshirts.json';
+ 
+function App() {
+  
+  return (
+    <div className="App">
+      <h1 id="titleShop">Tshirts Shop</h1>
+     <div className="cards">
+      
+      {tshirts.map((tshirt) => <Product imageAttribute={tshirt} titleAttribute={tshirt} manufactAttribute={tshirt} priceAttribute={tshirt} availableColors={tshirt.availableColors} /> )}
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
